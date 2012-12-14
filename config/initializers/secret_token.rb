9349d79894b859290857b93f3ffc1e6420a274ac
Tum Chaturapruek <Tum_Chaturapruek@HMC.Edu>
2012-12-14 00:22:11 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Math136::Application.config.secret_token = 'eb5f201e109fe13b431bf11976422b1680387c800e87f5f18e4d86541d0b635caf88ae32bdc514f685bb598782e934c4d550cbc73595aa5d8adba6363019240e'
